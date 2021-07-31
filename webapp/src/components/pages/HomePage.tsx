d8cd4d4fd1862282f16e449cf94a44fc0b0ea10e
Andrew Dimmer <contactandrewd@gmail.com>
2021-07-31 03:41:11 -0400

    feat: Add Basic Home Page Content

--- webapp/src/components/pages/HomePage.tsx
+++ webapp/src/components/pages/HomePage.tsx
@@ -1,10 +1,38 @@
+import { Typography } from "@material-ui/core";
+import { Info, Room, Search, Settings } from "@material-ui/icons";
 import React from "react";
+import { styles } from "../../styles";
 import PageTemplate from "../layouts/PageTemplate";
+import HomePageButton from "../misc/HomePageButton";
 
 declare interface HomePageProps {}
 
 const HomePage: React.FunctionComponent<HomePageProps> = ({}) => {
-  return <PageTemplate heading="Welcome to Bun Alert!" title=""></PageTemplate>;
+  const classes = styles();
+
+  return (
+    <PageTemplate heading="Welcome to Bun Alert!" title="">
+      <Typography variant="h6" className={classes.pageTitle}>
+        "They need to know: there are buns." - xkcd 1871
+      </Typography>
+      <HomePageButton
+        label="Learn More About Bun Alert"
+        route="/about"
+        icon={Info}
+      />
+      <HomePageButton
+        label="Report a Bun Sighting"
+        route="/report-bun"
+        icon={Room}
+      />
+      <HomePageButton
+        label="Find Nearby Buns"
+        route="/find-buns"
+        icon={Search}
+      />
+      <HomePageButton label="Settings" route="/settings" icon={Settings} />
+    </PageTemplate>
+  );
 };
 
 export default HomePage;
