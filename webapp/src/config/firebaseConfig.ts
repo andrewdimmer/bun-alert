c3220c030827dedcdfbb07489ca2070ca1e562ad
Andrew Dimmer <43498863+andrewdimmer@users.noreply.github.com>
2021-07-30 22:53:51 -0400

    chore: Initialize Bun Alert (#1)
    * chore: Initialize React App
    
    * chore: Install Material UI
    
    * chore: Configure Firebase Client

---
+++ webapp/src/config/firebaseConfig.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+export const firebaseApp = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  databaseUrl: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+});
+
+export const firestore = firebaseApp.firestore().collection("hackathons").doc("SelfieHacks2021");
