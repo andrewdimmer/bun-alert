ea56deed17c9833f36a6900e898a62e6e60fddd4
Andrew Dimmer <contactandrewd@gmail.com>
2021-07-31 02:49:59 -0400

    styles: Add Material UI Theme and Styles

---
+++ webapp/src/styles/useDarkMode.ts
@@ -0,0 +1,35 @@
+import React from "react";
+
+export const useDarkMode = () => {
+  const [theme, setTheme] = React.useState<"light" | "dark">("light");
+  const [componentMounted, setComponentMounted] = React.useState(false);
+  const setMode = (mode: "light" | "dark") => {
+    window.localStorage.setItem("theme", mode);
+    setTheme(mode);
+  };
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setMode("dark");
+    } else {
+      setMode("light");
+    }
+  };
+
+  React.useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as
+      | "light"
+      | "dark"
+      | null;
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches &&
+    !localTheme
+      ? setMode("dark")
+      : localTheme
+      ? setTheme(localTheme)
+      : setMode("light");
+    setComponentMounted(true);
+  }, []);
+
+  return { theme, toggleTheme, componentMounted };
+};
